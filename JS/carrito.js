6ba68647f6b45366063eb8be0699496a82972ea7
Arkanhell98 <gabopc98@gmail.com>
2025-07-07 15:58:13 -0500

    Actualizado

---
+++ JS/carrito.js
@@ -0,0 +1,72 @@
+let carrito = [];
+let total = 0;
+
+function agregarAlCarrito(nombre, precio) {
+  carrito.push({ nombre, precio });
+  total += precio;
+  mostrarCarrito();
+}
+
+function mostrarCarrito() {
+  const lista = document.getElementById("lista-carrito");
+  const totalElemento = document.getElementById("total");
+  lista.innerHTML = "";
+
+  carrito.forEach((producto, index) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+    li.innerHTML = `
+      ${producto.nombre} - $${producto.precio.toLocaleString()}
+      <button class="btn btn-danger btn-sm" onclick="eliminarDelCarrito(${index})">Eliminar</button>
+    `;
+    lista.appendChild(li);
+  });
+
+  totalElemento.textContent = `Total: $${total.toLocaleString()}`;
+}
+
+function eliminarDelCarrito(index) {
+  total -= carrito[index].precio;
+  carrito.splice(index, 1);
+  mostrarCarrito();
+}
+
+function vaciarCarrito() {
+  carrito = [];
+  total = 0;
+  mostrarCarrito();
+}
+
+
+function finalizarCompra() {
+    if (carrito.length === 0) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Tu carrito está vacío',
+        imageUrl: '/Imagenes/Logo negro.png',
+        imageWidth: 90, 
+        imageHeight: 40, 
+        showConfirmButton: false,
+        timer: 1500
+      });
+      return;
+    }
+  
+    Swal.fire({
+      title: '¡Gracias por tu compra!',
+      text: `Total pagado: $${total.toLocaleString()}`,
+      icon: 'success',
+      imageUrl: '/Imagenes/Logo negro.png',
+        imageWidth: 90, 
+        imageHeight: 40,
+      confirmButtonText: 'Aceptar',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    });
+  
+    vaciarCarrito(); // Esto limpia el carrito después de comprar
+  }
---
+++ README.md
@@ -0,0 +1,22 @@
+Readme
+
+URBN Sportwear es una landing page moderna y responsive para una tienda de artículos deportivos. El objetivo principal es atraer clientes potenciales, mostrar productos destacados y dirigirlos hacia una experiencia de compra completa, este proyecto es realizado por alumnos apasionados por la programación, buscando crear una página que le brinde al usuario una experiencia amable tanto para las personas expertas, como para los principiantes. 
+
+Características
+•	Diseño atractivo y responsive.
+•	Interfaz amable con el usuario. 
+•	 Sección de productos destacados.
+•	Opiniones de nuestros clientes.
+•	Formulario de suscripción.
+•	 Carga rápida y navegación fluida.
+•	Chatbot integrado.
+
+Tecnologías usadas
+•	HTML5.
+•	CSS3.
+•	JavaScript.
+•	Bootstrap.
+
+
+
+
